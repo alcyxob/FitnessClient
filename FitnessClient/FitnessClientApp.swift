0fc034e400c7eee5ec237b7a6fded03645661fca
Oleksandr Vorobiov <alcyxob@gmail.com>
2025-05-06 11:16:12 +0300

    Initial Commit

---
+++ FitnessClient/FitnessClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessClientApp.swift
+//  FitnessClient
+//
+//  Created by Oleksandr Vorobiov on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessClientTests/FitnessClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessClientTests.swift
+//  FitnessClientTests
+//
+//  Created by Oleksandr Vorobiov on 06.05.2025.
+//
+
+import Testing
+@testable import FitnessClient
+
+struct FitnessClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
